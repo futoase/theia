073b9e8ca90cc8a9e7bad5ea9fd382b02a37cf7d
Vitaliy Guliy <vguliy@codenvy.com>
2018-04-17 18:00:44 +0300

    Refactoring of editorconfig service

--- packages/editorconfig/src/node/editorconfig-service-impl.ts
+++ packages/editorconfig/src/node/editorconfig-service-impl.ts
@@ -5,8 +5,7 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { injectable, inject } from "inversify";
-import { WorkspaceServer } from "@theia/workspace/lib/common";
+import { injectable } from "inversify";
 import * as editorconfig from 'editorconfig';
 import { KnownProps } from "editorconfig";
 import { EditorconfigService } from "../common/editorconfig-interface";
@@ -14,32 +13,8 @@ import { EditorconfigService } from "../common/editorconfig-interface";
 @injectable()
 export class EditorconfigServiceImpl implements EditorconfigService {
 
-    constructor(
-        @inject(WorkspaceServer) protected readonly workspaceServer: WorkspaceServer
-    ) { }
-
     getConfig(file: string): Promise<KnownProps> {
-        return new Promise((resolve, reject) => {
-            this.workspaceServer.getRoot().then(root => {
-                if (root) {
-                    if (root.startsWith("file://")) {
-                        root = root.substring("file://".length);
-                    }
-
-                    editorconfig.parse(file)
-                        .then(config => {
-                            resolve(config);
-                        }).catch(reason => {
-                            reject(reason);
-                        });
-                } else {
-                    reject();
-                }
-            }).catch(reason => {
-                reject(reason);
-            });
-
-        });
+        return editorconfig.parse(file);
     }
 
 }
